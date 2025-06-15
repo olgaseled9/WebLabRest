0f632d2962b05e6c6d922690bb1fb90ff8b510fd
Olga Seledtsova <olgaseledtsova9@yandex.by>
2025-06-15 22:42:40 +0300

    lab9

--- WebLabBlazor/Program.cs
+++ WebLabBlazor/Program.cs
@@ -1,28 +1,40 @@
 using WebLabBlazor.Components;
+using WebLabBlazor.Services;
+using WebLabRest.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient<IProductService<Dish>, ApiProductService>(client => 
+{
+    client.BaseAddress = new Uri("https://localhost:7002"); // Адрес вашего API
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseStaticFiles(); 
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+builder.Services.AddCors(options => options.AddPolicy("AllowBlazor", policy => 
+{
+    policy.WithOrigins("https://localhost:5001") 
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+}));
+
+// После app.UseRouting();
+app.UseCors("AllowBlazor");
