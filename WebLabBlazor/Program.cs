d827a6dbfbc68af7683d8456c203ff829479b310
Olga Seledtsova <olgaseledtsova9@yandex.by>
2025-06-15 22:59:42 +0300

    lab9

--- WebLabBlazor/Program.cs
+++ WebLabBlazor/Program.cs
@@ -4,13 +4,20 @@ using WebLabRest.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddPolicy("AllowBlazor", policy => 
+{
+    policy.WithOrigins("https://localhost:5001") 
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+}));
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient<IProductService<Dish>, ApiProductService>(client => 
-{
-    client.BaseAddress = new Uri("https://localhost:7002"); // Адрес вашего API
-});
+builder.Services
+    .AddHttpClient<IProductService<Dish>, ApiProductService>(c =>
+        c.BaseAddress = new Uri("https://localhost:7002/api/dishes"));
+
 
 var app = builder.Build();
 
@@ -22,19 +29,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); 
+
+app.UseRouting();
+app.UseCors("AllowBlazor");
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-
-builder.Services.AddCors(options => options.AddPolicy("AllowBlazor", policy => 
-{
-    policy.WithOrigins("https://localhost:5001") 
-        .AllowAnyHeader()
-        .AllowAnyMethod();
-}));
-
-// После app.UseRouting();
-app.UseCors("AllowBlazor");
